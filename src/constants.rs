598494d7d0214ca967dc793803ce1a1ab016cca2
bo0tzz <git@bo0tzz.me>
2024-09-09 21:49:51 +0200

    Tuning

--- src/constants.rs
+++ src/constants.rs
@@ -3,13 +3,13 @@
 use crate::types::VelocityScaleFactors;
 
 pub const SCALE_FACTORS: VelocityScaleFactors = VelocityScaleFactors {
-    x: 1.0,
-    y: 1.0,
-    z: 0.5,
-    feedrate: 0.4
+    x: 0.5,
+    y: 0.5,
+    z: 0.3,
+    feedrate: 1.0
 };
 
-pub const PRINTER_UPDATE_RATE_HZ: f32 = 30.0;
+pub const PRINTER_UPDATE_RATE_HZ: f32 = 5.0;
 pub const PRINTER_TIME_STEP: f32 = 1.0 / PRINTER_UPDATE_RATE_HZ;
 pub fn printer_update_interval() -> std::time::Duration {
     std::time::Duration::from_secs_f32(1.0 / PRINTER_UPDATE_RATE_HZ)
